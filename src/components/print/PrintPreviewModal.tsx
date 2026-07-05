8ff10a2baf6590ed50b79894683ed2ec278f0d1d
kshetrihari09-dev <kshetrihari09@gmail.com>
2026-07-05 11:42:33 +0545

    previewmodalfixed

--- src/components/print/PrintPreviewModal.tsx
+++ src/components/print/PrintPreviewModal.tsx
@@ -196,7 +196,7 @@ export default function PrintPreviewModal({
             style={{
               position: 'fixed',
               top:       effectiveFullscreen ? 0 : '0%',
-              left:      effectiveFullscreen ? 0 : '0%',
+              left:      effectiveFullscreen ? 0 : '-50%',
               transform: effectiveFullscreen ? 'none' : 'translate(0%, 70%)',
               width:     effectiveFullscreen ? '100vw' : isTablet ? 'min(880px, 96vw)' : 'min(1000px, 98vw)',
               height:    effectiveFullscreen ? '100dvh' : 'auto',
