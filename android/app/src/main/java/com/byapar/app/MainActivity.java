b1d53f5e9628d104139fb54b0071d918d2424a05
kshetrihari09-dev <kshetrihari09@gmail.com>
2026-07-06 19:30:54 +0545

    Add Capacitor for Android app

---
+++ android/app/src/main/java/com/byapar/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.byapar.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.byapar.app',
+  appName: '\x16Byapar',
+  webDir: 'dist'
+};
+
+export default config;
